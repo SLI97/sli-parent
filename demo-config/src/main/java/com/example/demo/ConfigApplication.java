1c8269c335f7bdacd60d3e5df2b4caf0b1b16883
liangcg <liangcg@east-sw.com>
2020-07-09 19:13:11 +0800

    init

--- demo-config/src/main/java/com/example/demo/ConfigApplication.java
+++ demo-config/src/main/java/com/example/demo/ConfigApplication.java
@@ -1,14 +1,42 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan({"com.example"})
+@EnableDiscoveryClient
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
 
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RestController
+    class HelloController {
+
+        @Autowired
+        private RestTemplate restTemplate;
+
+        @RequestMapping(value = "/hello", method = RequestMethod.GET)
+        public String hello() {
+            return "hello config123";
+//            return restTemplate.getForObject("http://DEMO-GATEWAY/gateway/hello", String.class);
+        }
+    }
 }
--- demo-gateway/src/main/java/com/example/demo/GatewayApplication.java
+++ demo-gateway/src/main/java/com/example/demo/GatewayApplication.java
@@ -1,20 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @ComponentScan({"com.example"})
-//@EnableZuulProxy
-//@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
@@ -32,5 +37,13 @@ public class GatewayApplication {
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
         bean.setOrder(0);
         return bean;
+    }
+
+    @RestController
+    class HelloController {
+        @RequestMapping(value = "/hello", method = RequestMethod.GET)
+        public String hello() {
+            return "haha2344";
+        }
     }
 }
